3f6ea66f692f0bcd707ccb251ecc4c3b136ec3f9
yubinCloud <yubin_SkyWalker@yeah.net>
2022-02-16 21:28:29 +0800

    #dev first commit

---
+++ .vuepress/config.ts
@@ -0,0 +1,52 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  title: 'notebook of front-end',
+  description: 'Just playing around',
+  theme: 'reco',
+  base: '/notebook-front/',
+  head: [
+    ['link', { rel: 'icon', href: '/images/favicon.ico' }],
+  ],
+  themeConfig: {
+    style: '@vuepress-reco/style-default',
+    logo: '/images/logo.png',
+    author: 'yubin',
+    docsRepo: 'https://github.com/yubinCloud/notebook-front',
+    docsBranch: 'main',
+    docsDir: 'notebook-front',
+    lastUpdatedText: '',
+    // series 为原 sidebar
+    series: {
+      '/docs/base/': [
+        {
+          text: 'HTML',
+          children: ['HTML 基础.html']
+        }
+      ]
+    },
+    navbar: [
+      { text: 'notebook', link: 'https://yubincloud.github.io/notebook' },
+      { text: '首页', link: '/' },
+      {
+        text: '前端基础',
+        children: [
+          { text: 'HTML', link: '/docs/base/HTML 基础.html' }
+        ]
+      },
+    ],
+    
+    // valineConfig 配置与 1.x 一致
+    // valineConfig: {
+    //   appId: 'xxx',
+    //   appKey: 'xxx',
+    //   placeholder: '填写邮箱可以收到回复提醒哦！',
+    //   verify: true, // 验证码服务
+    //   // notify: true,
+    //   recordIP: true,
+    //   // hideComments: true // 隐藏评论
+    // },
+  },
+  // debug: true,
+})
---
+++ README.md
@@ -0,0 +1,25 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - MdContent
+  - Footer
+bannerBrand:
+  heroImage: /images/logo.png
+  heroImageStyle:
+    maxWidth: '200px'
+    width: '100%'
+    display: block
+    margin: '0 auto 2rem'
+    borderRadius: '1rem'
+  bgImage: '/images/bg.svg'
+  heroText: notebook of front-end
+  tagline: 前端的升级打怪之路
+  buttons:
+    - { text: Start, link: '/docs/base/HTML 基础.html' }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
+
+
